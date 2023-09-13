ceed629db52daf8b87840fb85524c5a88fdf479a
Andreia <alexandraper12@hotmail.com>
2023-09-13 19:18:00 +0100

    feat: added dotenv and created get request to retrieve api data

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,32 @@
+require('dotenv').config();
 var express = require('express');
+const axios = require('axios');
 var app = express();
 
 const port = process.env.PORT || 3000;
+const apiKey = process.env.openWeatherAPIKey;
 
-app.get('/', function (req, res) {
-    res.status(200).json({
-        result: 'WeatherApp'
-      });
-  });
+app.get("/weather/:city", async (req, res) => {
+  try {
+    console.log(req.params);
+    const city_id = req.params.city;
+    console.log(apiKey)
+
+    const response = await axios.get(
+      `http://api.openweathermap.org/data/2.5/forecast?id=${city_id}&appid=${apiKey}`
+    );
+
+    const weatherData = response.data;
+
+    res.json(weatherData);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching weather data." });
+  }
+});
 
 app.listen(port, function () {
-    console.log('App is running at port:', port);
-  });
+  console.log("App is running at port:", port);
+});
